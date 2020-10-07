15772f3ee72c81c8a7c1eaecf18359bbdc003fbc
Phil McCarthy <philmcc2631@gmail.com>
2020-10-07 13:11:20 -0600

    Add closing time and add dish methods to restaurant to complete iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,20 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes
+  attr_reader :opening_time, :name, :dishes, :closing_time
 
   def initialize(opening_time, name)
     @opening_time = opening_time
+    @closing_time = ''
     @name = name
     @dishes = []
+  end
+
+  def closing_time(hours)
+    @closing_time = @opening_time.to_i
+    @closing_time += hours
+    @closing_time.to_s + ':00'
+  end
+
+  def add_dish(dish)
+    @dishes << dish
   end
 end
